5786665ee5efa8971691625d6f266fa007eb1d42
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2021-12-17 20:42:24 +0800

    [Route] added new routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,11 @@
 import Home from "./components/home";
-import Registration from "./components/registration";
+import CFRegistration from "./components/registration/casefile";
+import HRegistration from "./components/registration/heist";
+import ERRegistration from "./components/registration/escaperoom";
+import Promo from "./components/promo";
+import CFGame from "./components/game/casefile";
+import HGame from "./components/game/heist";
+import ERGame from "./components/game/escaperoom";
 
 export const routes = [
   {
@@ -10,8 +16,44 @@ export const routes = [
   },
   {
     id: 2,
-    name: "Registration",
-    component: <Registration />,
-    path: "/registration",
+    name: "Case File Registration",
+    component: <CFRegistration />,
+    path: "/casefile/registration",
+  },
+  {
+    id: 3,
+    name: "Heist Registration",
+    component: <HRegistration />,
+    path: "/heist/registration",
+  },
+  {
+    id: 4,
+    name: "Escape Room Registration",
+    component: <ERRegistration />,
+    path: "/escaperoom/registration",
+  },
+  {
+    id: 5,
+    name: "Promo",
+    component: <Promo />,
+    path: "/promotion",
+  },
+  {
+    id: 6,
+    name: "Case File Game",
+    component: <CFGame />,
+    path: "/casefile",
+  },
+  {
+    id: 7,
+    name: "Heist Game",
+    component: <HGame />,
+    path: "/heist",
+  },
+  {
+    id: 8,
+    name: "Escape Room Game",
+    component: <ERGame />,
+    path: "/escaperoom",
   },
 ];
